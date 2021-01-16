a705f02d2be8085b911460988bd5a6856ba1384b
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2021-01-16 20:18:46 +0900

    add animation in HOME page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,38 @@
 @extends('template')
-@section('css_address','css/about.css')
+@section('css_address','css/home.css')
+@section('right_contents')
+  <div class="wrap">
+    <div class="content-text">
+      <span>P</span>
+      <span>l</span>
+      <span>e</span>
+      <span>a</span>
+      <span>s</span>
+      <span>e</span>
+      <span> </span>
+      <span>S</span>
+      <span>e</span>
+      <span>l</span>
+      <span>e</span>
+      <span>c</span>
+      <span>t</span>
+    </div>
+    <div class="content-text-menu">
+      MENU
+    </div>
+    <div class="content-text">
+      <span>O</span>
+      <span>n</span>
+      <span> </span>
+      <span>L</span>
+      <span>e</span>
+      <span>f</span>
+      <span>t</span>
+      <span> </span>
+      <span>S</span>
+      <span>i</span>
+      <span>d</span>
+      <span>e</span>
+    </div>
+  </div>
+@endsection
