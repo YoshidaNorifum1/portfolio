5277a5418c13ff146697904ff0f8090e699e8d6f
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2021-01-16 09:26:59 +0900

    add fadein animation to ABOUT page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,29 +4,33 @@
   <div class="right_contents_title">
     About
   </div>
-  <div class="right_contents_main-container">
-    <div class="right_contents_main-title">
-      ・2002 - 2007
+  <div class="wrap">
+    <div class="right_contents_main-container">
+      <div class="right_contents_main-title">
+        ・2002 - 2007
+      </div>
+      <div class="right_contents_main-discription">
+        化学系高専。工学的な思考を学ぶ。
+      </div>
     </div>
-    <div class="right_contents_main-discription">
-      化学系高専。工学的な思考を学ぶ。
+    <div class="right_contents_main-container">
+      <div class="right_contents_main-title">
+        ・2012 - 現在
+      </div>
+      <div class="right_contents_main-discription">
+        薬学部へ進み、卒業後薬局薬剤師として勤務。
+        一般的な薬剤師業務に加え、MS Access/VBAを使用した在庫管理アプリや、
+        Pythonを使用したレセプトファイルからの処方情報抽出ツールの開発など。
+      </div>
+    </div>
+    <div class="right_contents_main-container">
+      <div class="right_contents_main-title">
+        ・2017 - 現在
+      </div>
+      <div class="right_contents_main-discription">
+        android用ホームアプリやPythonを用いたスクレイピングツールなどの個人開発。
+      </div>
     </div>
   </div>
-  <div class="right_contents_main-container">
-    <div class="right_contents_main-title">
-      ・2012 - 現在
-    </div>
-    <div class="right_contents_main-discription">
-      薬学部へ進み、卒業後薬局薬剤師として勤務。
-      一般的な薬剤師業務に加え、MS Access/VBAを使用した在庫管理アプリや、
-      Pythonを使用したレセプトファイルからの処方情報抽出ツールの開発など。
-    </div>
-  </div>
-  <div class="right_contents_main-container">
-    <div class="right_contents_main-title">
-      ・2017 - 現在
-    </div>
-    <div class="right_contents_main-discription">
-      android用ホームアプリやPythonを用いたスクレイピングツールなどの個人開発。
 
 @endsection
