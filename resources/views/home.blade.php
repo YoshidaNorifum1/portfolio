cca642ce1e3c388a2dfa8b46737f7105d97a429b
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2021-02-18 17:10:10 +0900

    edit HOME text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,36 @@
   <div class="wrap">
     <div class="content-text">
       <span>P</span>
+      <span>o</span>
+      <span>s</span>
+      <span>h</span>
+      <span>i</span>
+      <span>d</span>
+      <span>a</span>
+      <span> </span>
+      <span>N</span>
+      <span>o</span>
+      <span>r</span>
+      <span>i</span>
+      <span>f</span>
+      <span>u</span>
+      <span>m</span>
+      <span>i</span>
+      <span>'</span>
+      <span>s</span>
+      <span> </span>
+      <span>P</span>
+      <span>o</span>
+      <span>r</span>
+      <span>t</span>
+      <span>f</span>
+      <span>o</span>
+      <span>l</span>
+      <span>i</span>
+      <span>o</span>
+    </div>
+    <div class="content-text">
+      <span>P</span>
       <span>l</span>
       <span>e</span>
       <span>a</span>
@@ -23,6 +53,21 @@
     </div>
     </a>
     <div class="content-text">
+      <span>O</span>
+      <span>r</span>
+      <span> </span>
+      <span>S</span>
+      <span>e</span>
+      <span>l</span>
+      <span>e</span>
+      <span>c</span>
+      <span>t</span>
+      <span> </span>
+      <span>M</span>
+      <span>e</span>
+      <span>n</span>
+      <span>u</span>
+      <span> </span>
       <span>O</span>
       <span>n</span>
       <span> </span>
